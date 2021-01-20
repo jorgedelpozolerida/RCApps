b6a4ecbf1b32942175a52f963224c79a28eb34bb
shinyuserqiagen <shinyuserqiagen@gmail.com>
2021-01-20 08:38:32 +0000

    htmldocsmanager added

---
+++ apps/htmldocsManager_app/server.R
@@ -0,0 +1,114 @@
+## ---------------------------
+##
+## Script Name:  
+##
+## Purpose of script: SERVER FOR REPORT GENERATOR APP
+##
+## Author: jorgedelpozolerida
+##
+## Date Created: 2021-01-15
+##
+## Email: jorgedelpozolerida@gmail.com
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+#' TO DO:
+#' Make sure it is necessary to source files twice or just in ui function
+#' Make sure data table is changing reactively
+
+
+
+# Define necessary variables  and import libraries------------------------------
+
+pathsjson_dir <- file.path("/home/shinyuser/RCApps/info/paths.json")
+
+project_dir <- jsonlite::read_json(pathsjson_dir)$RCApps_project
+
+pathtohtmls <- file.path(
+  jsonlite::read_json(pathsjson_dir)$htmlfiles_absolute
+)
+
+pathtoRfiles <- file.path(
+  jsonlite::read_json(pathsjson_dir)$RCApps_project,
+  "/R"
+)
+
+sapply(list.files(pathtoRfiles, full.names = TRUE), source)
+
+
+
+# Server function ------------------------------------------- ------------------
+
+server <- function(input, output) {
+  
+  set.seed(122)
+  histdata <- rnorm(500)
+  
+  summarydata_r <- reactive({ func_gethtmldocssummary(pathtohtmls) })
+  htmldataframe_r <- reactive({  summarydata_r()$data })
+  
+  t <- mod_dataframeexplorerServer('htmldataframe', htmldataframe_r)
+  output$text <- renderText({
+    t()
+  })
+  
+  mod_docmanagerServer("addfile")
+  
+  # --------------------- OUTPUT INFO BOXES ---------------------------------
+  
+  output$n_html <- renderInfoBox({
+    infoBox("Number of HTML files", 
+            icon = icon("file", lib = "glyphicon"), color = "purple",
+            value=as.character(summarydata_r()$n_html)
+    )
+  })
+  output$n_authors <- renderInfoBox({
+    infoBox("Number of authors", 
+            icon = icon("user", lib = "glyphicon"), color = "blue",
+            value=as.character(summarydata_r()$n_authors)
+    )
+  })
+  output$approvalBox <- renderInfoBox({
+    infoBox(
+      "Approval", "80%", icon = icon("thumbs-up", lib = "glyphicon"),
+      color = "yellow"
+    )
+  })
+  
+  # --------------------- OUTPUT DROPDOWN MENU---------------------------------
+  
+  # Messages
+  messageData <- data.frame(from=c('App developer'), message=c('App under development'))
+  
+  output$messageMenu <- renderMenu({
+    # messageData is a data frame with two columns, 'from' and 'message'.
+    msgs <- apply(messageData, 1, function(row) {
+      messageItem(from = row[["from"]], message = row[["message"]])
+    })
+  })
+  
+  # Notifications
+  notificationData <- data.frame(text=c('notification1')) 
+  output$notificationMenu <- renderMenu({
+    # notificationData is a data frame with one columns 'text'
+    notif <- apply(notificationData, 1, function(row) {
+      notificationItem(text = row[["text"]])
+    })
+  })
+  
+  # Tasks
+  taskData <- data.frame(text=c('task1'), value=c(90), color=c('green') ) 
+  output$taskMenu <- renderMenu({
+    # taskData is a data frame with three columns 'text', 'value', 'color'
+    notif <- apply(taskData, 1, function(row) {
+      taskItem(text = row[["text"]], color = row[["color"]], value = row[["value"]])
+    })
+    
+  })
+  
+}
+
---
+++ apps/htmldocsManager_app/ui.R
@@ -0,0 +1,145 @@
+## ---------------------------
+##
+## Script Name:  
+##
+## Purpose of script: UI FOR REPORT GENERATOR APP
+##
+## Author: jorgedelpozolerida
+##
+## Date Created: 2021-01-15
+##
+## Email: jorgedelpozolerida@gmail.com
+##
+## ---------------------------
+##
+## Notes: Dashboardpage for ui is composed of three elements: 
+## -header
+## -sidebar
+## -body 
+##   
+##
+## ---------------------------
+#' TO DO:
+#'
+#'
+
+# Define necessary variables  and import libraries------------------------------
+
+# pathsjson_dir <- file.path("../../info/paths.json")
+# 
+# project_dir <- jsonlite::read_json(pathsjson_dir)$RCA_rmarkdowns_project
+# 
+# pathtohtmls <- file.path(
+#   jsonlite::read_json(pathsjson_dir)$htmlfiles_absolute
+# )
+# 
+# pathtoRfiles <- file.path(
+#   jsonlite::read_json(pathsjson_dir)$RCApps_project,
+#   "/R"
+# )
+
+sapply(list.files("../../R", full.names = TRUE), source)
+
+
+
+# Header ---------------------------------------------------------------------------
+
+
+header <- dashboardHeader(
+  title="RCA markdowns Explorer",
+  # title = shinyDashboardLogo(
+  #   theme = "blue_gradient",
+  #   boldText = "RCA",
+  #   mainText = "markdowns Explorer",
+  #   # badgeText = ""
+  # ),
+  dropdownMenu(dropdownMenuOutput("messageMenu"), type='message'),
+  dropdownMenu(dropdownMenuOutput("notificationMenu"), type='notifications'),
+  dropdownMenu(dropdownMenuOutput("taskMenu"), type='tasks')
+)
+
+
+# Siderbar ------------------------------------------------------------------
+
+
+
+sidebar <- dashboardSidebar(
+  # id = "sidebar", # necessary in case one want to bookmark and restore seleted tab
+  sidebarSearchForm(textId = "searchText", buttonId = "searchButton",
+                    label = "Search..."),
+  sidebarMenu(
+    id="idebarmenu",
+    menuItem("Summary", tabName = "dashboard", icon = icon("book")),
+    menuItem("Add file", tabName = "widgets", icon = icon("book-open"))
+  )
+)
+
+
+# Body -------------------------------------------------------------------------
+
+
+body <- dashboardBody(
+  
+  # Enable shinyjs
+  shinyjs::useShinyjs(),
+  
+  #------------------------------ SET THEME ------------------------------------
+  
+  shinyDashboardThemes(
+    theme = "blue_gradient"
+  ),
+  
+  #------------------------------ TAB ITEMS ------------------------------------
+  
+  tabItems(
+    # Dashboard tab
+    tabItem(
+      tabName = "dashboard",
+      fillPage(
+      fluidRow(
+        # Dynamic infoBoxes
+        infoBoxOutput("n_html"),
+        infoBoxOutput("n_authors"),
+        infoBoxOutput("approvalBox")
+      ),
+      fluidRow(
+        box(
+          title="List of HTML files", status='primary', # can also be success/warning/info/danger/primary
+          solidHeader = TRUE, collapsible = TRUE, width=NULL,
+          #background = "maroon",
+          # module inside
+          mod_dataframeexplorerUI('htmldataframe'),
+
+        # tabBox(
+        #   title = tagList(shiny::icon("gear"), "tabBox"),
+        #   id='tabs',
+        #   side = "right",
+        #   height = "250px",
+        #   selected = "Tab3",
+        #   tabPanel("Tab1", "Tab content 1"),
+        #   tabPanel("Tab2", "Tab content 2"),
+        #   tabPanel("Tab3", "Note that when side=right, the tab order is reversed.")
+        # ),
+        # box(width = 4, actionButton("count", "Increment progress")
+        )
+        
+        ))
+    ),
+
+    # Widgets tab 
+    tabItem(
+      tabName = "widgets",
+      
+      mod_docmanagerUI("addfile")
+      )
+  )
+)
+
+
+
+
+
+
+# UI function ------------------------------------------------------------------
+
+ui <- dashboardPage(header, sidebar, body)
